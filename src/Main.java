ff195e4d4f7bf90d9a553ee140ac9e3d1f7a572d
eurus1108 <99813212+eurus1108@users.noreply.github.com>
2023-12-09 22:38:03 +0800

    feat: Create mock logic for auth validation
    This code simulate what auth validation will look later. I also test if the new fields are working as intended

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
+import data.CustomerRepo;
+import models.Customer;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your account number: ");
+        String accountNo = scanner.nextLine();
+
+        Customer currentCustomer = null;
+
+        for (Customer customer : CustomerRepo.getAllCustomer()) {
+            if (customer.getAccountNo().equals(accountNo)) {
+                currentCustomer = customer;
+                break;
+            }
+        }
+
+        if (currentCustomer == null) {
+            System.exit(0);
+        }
+
+        while (currentCustomer.getPinAttempts() < 3) {
+            if (currentCustomer.getPinAttempts() + 1 == 3) {
+                currentCustomer.setCaptured(true);
+            }
+
+            System.out.print("Enter you pin: ");
+            String pin = scanner.nextLine();
+
+            if (currentCustomer.getPin().equals(pin)) {
+                break;
+            }
+
+            System.out.println("Count: " + currentCustomer.getPinAttempts());
+            currentCustomer.setPinAttempts(currentCustomer.getPinAttempts() + 1);
+        }
+
+        System.out.println("Is captured?: " + currentCustomer.isCaptured());
     }
 }
