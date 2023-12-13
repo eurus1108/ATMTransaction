652cf9b215356e2b2b68c47feda07adcebcc3c52
eurus1108 <99813212+eurus1108@users.noreply.github.com>
2023-12-13 16:22:25 +0800

    feat: Change data type to BigDecimal
    I found out that BigDecimal is the appropriate data type for currencies.

--- src/data/CustomerRepo.java
+++ src/data/CustomerRepo.java
@@ -2,6 +2,7 @@ package data;
 
 import models.Customer;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,11 +10,11 @@ public class CustomerRepo {
     private static List<Customer> customers = new ArrayList<>();
 
     static {
-        customers.add(new Customer("0123-4567-8901", "Roel Richard", 5_000, "1111"));
-        customers.add(new Customer("2345-6789-0123", "Dorie Marie", 0, "2222"));
-        customers.add(new Customer("3456-7890-1234", "Railee Darrel", 10_000, "3333"));
-        customers.add(new Customer("4567-8901-2345", "Railynne Dessirei", 2_500, "4444"));
-        customers.add(new Customer("5678-9012-3456", "Raine Dessirei", 10_000, "5555"));
+        customers.add(new Customer("0123-4567-8901", "Roel Richard", new BigDecimal(5_000), "1111"));
+        customers.add(new Customer("2345-6789-0123", "Dorie Marie", new BigDecimal(0), "2222"));
+        customers.add(new Customer("3456-7890-1234", "Railee Darrel", new BigDecimal(10_000), "3333"));
+        customers.add(new Customer("4567-8901-2345", "Railynne Dessirei", new BigDecimal(2_500), "4444"));
+        customers.add(new Customer("5678-9012-3456", "Raine Dessirei", new BigDecimal(10_000), "5555"));
     }
 
     public static List<Customer> getAllCustomer() {
