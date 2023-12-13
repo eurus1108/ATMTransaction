fcbf0e1d2d95ef40a5c013470d72610b8bb82129
eurus1108 <99813212+eurus1108@users.noreply.github.com>
2023-12-13 23:24:49 +0800

    refactor: Modify accountNo to final

--- src/models/Customer.java
+++ src/models/Customer.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class Customer {
-    private String accountNo;
+    private final String accountNo;
     private String name;
     private BigDecimal balance;
     private String pin;
